9141d960cea97a0b89c692addf70ba541560c530
rayaso-0 <180339525+rayaso-0@users.noreply.github.com>
2024-09-15 17:09:10 -0400

    stage 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
-const { Client, IntentsBitField } = require('discord.js');
+require('dotenv').config();
+const { Client, IntentsBitField, messageLink } = require('discord.js');
 
 const client = new Client({
     intents: [
-        IntentsBitField.Flags.Guilds
-    ]
-})
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`✅ ${c.user.tag} is online.`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === 'hello') {
+        message.reply('im gonna touch you @(473203135007948820)')
+    }
+});
+
+client.login(process.env.TOKEN);
+
