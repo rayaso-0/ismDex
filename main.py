f3318db5a4c32a11490830498bebdc511471501d
rayaso-0 <180339525+rayaso-0@users.noreply.github.com>
2024-09-15 10:44:31 -0400

    run bot test

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import random
 import time
 import os
 from token_1 import *
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("ismDex is ready!")
+
+bot.run(bot_token)
+
+
+
 
 
 
